bc6af9f2c9954f8b79539e0f52b4cdfcc3c32370
vazenin <cnikromen@gmail.com>
2026-04-13 16:15:53 +0300

    fix: use absolute paths for huggingface models

--- modalities/spatial.py
+++ modalities/spatial.py
@@ -28,9 +28,12 @@ def preprocess_sam(x, img_size=1024):
     return x
 
 class SpatialSIDADetector(BaseModality):
-    def __init__(self, model_path: str = "../SIDA/ck/SIDA-13B-description"):
+    def __init__(self, model_path: str = None):
+        if model_path is None:
+            model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../SIDA/ck/SIDA-7B-description'))
+            
         super().__init__()
-        print("[spatial] Инициализация SIDA (RGB) в 8-bit...")
+        print(f"[spatial] Инициализация SIDA (RGB) в 8-bit... Путь к весам: {model_path}")
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.vis_save_path = "./sida_masks_output"
         os.makedirs(self.vis_save_path, exist_ok=True)
